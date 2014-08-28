0c4a36e8c0c1475f4be55caa623f20dc1dd2c4f6
Shubhamoy Chakrabarty <shubhamoy@kalakhatta.com>
2014-08-28 17:57:19 +0530

    Multiple Changes
    Deleted angular.min.js & replaced with CDN
    Data Presentation Changes

--- src/data.js
+++ src/data.js
@@ -2,16 +2,28 @@ var myApp = angular.module('boerickeSearch', []);
 myApp.factory('LinkMedicineSympton', function(){
 	var LinkMedicineSympton = {};
 
-	LinkMedicineSympton.cast = [
+	LinkMedicineSympton = [
   {
     "link":"http://homeoint.org/books/boericmm/a/abies-c.htm",
     "name":"ABIES CANADENSIS-PINUS CANADENSIS",
-    "symptom":"Head, Stomach, Female, Fever"
+    "symptom":[
+    			{1:"Head"},
+    			{2:"Stomach"},    			
+    			{3:"Female"}, 
+    			{4:"Fever"},
+    		]
   },
   {
     "link":"http://homeoint.org/books/boericmm/a/abies-n.htm",
     "name":"ABIES NIGRA",
-    "symptom":"Head, Stomach, Chest, Back, Sleep, Fever"
+    "symptom":[
+    			{1:"Head"},
+    			{2:"Stomach"},    			
+    			{3:"Chest"},    			    			
+    			{4:"Back"},    			    			
+    			{5:"Sleep"}, 
+    			{6:"Fever"},
+    		]
   },
 
 ]
